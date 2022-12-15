31b3cbc464aa4cef19a0856b3613dedb4ca6f536
jayukasode23 <jayukasode2398@gmail.com>
2022-12-15 13:25:13 +0530

    Added Employee Details

---
+++ Management.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public void TopRecords(List<ProductReview> review)
+        {
+            var recordData = (from productReview in review
+                              orderby productReview.Rating descending
+                              select productReview).Take(3);
+
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+
+
+        }
+    }
+}
+   
